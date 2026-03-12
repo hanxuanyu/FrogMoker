4623e1457f1e93042e992e2d846f3d3b393416dd
hanxuanyu <2252193204@qq.com>
2026-03-12 19:13:44 +0800

    springboot项目初始化

--- src/main/java/com/hxuanyu/frogmoker/FrogMokerApplication.java
+++ src/main/java/com/hxuanyu/frogmoker/FrogMokerApplication.java
@@ -1,4 +1,4 @@
-package com.example.frogmoker;
+package com.hxuanyu.frogmoker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
